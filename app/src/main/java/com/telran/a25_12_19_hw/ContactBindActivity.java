20d73987b3250541b388eaf6fdf7e5d918da2bcd
Gregory Sheygam <sheygam@gmail.com>
2019-12-26 18:02:07 +0200

    lesson 26/12/19

--- app/src/main/java/com/telran/a25_12_19_hw/ContactBindActivity.java
+++ app/src/main/java/com/telran/a25_12_19_hw/ContactBindActivity.java
@@ -2,12 +2,17 @@ package com.telran.a25_12_19_hw;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.telran.a25_12_19_hw.databinding.ActivityContactBindBinding;
 
-public class ContactBindActivity extends AppCompatActivity {
+public class ContactBindActivity extends AppCompatActivity implements View.OnClickListener /*, ContactViewModel.ViewModelListener*/ {
 
     private ActivityContactBindBinding binding;
 
@@ -22,6 +27,7 @@ public class ContactBindActivity extends AppCompatActivity {
                     "",
                     "",
                     -1);
+
         }else{
             viewModel = new ContactViewModel(getIntent().getStringExtra("NAME"),
                     getIntent().getStringExtra("EMAIL"),
@@ -30,6 +36,91 @@ public class ContactBindActivity extends AppCompatActivity {
                     getIntent().getIntExtra("POSITION",-1));
         }
 
-        binding. setViewModel(viewModel);
+//        viewModel.setListener(this);
+//        viewModel.setListener(bundle -> {
+//            Intent intent = new Intent();
+//            intent.putExtras(bundle);
+//            setResult(RESULT_OK,intent);
+//            finish();
+//        });
+        viewModel.liveData.observe(this, bundle -> {
+            load();
+            if(!bundle.getString("ACTION").equals("REMOVE")){
+                save(bundle.getString("NAME"),bundle.getString("PHONE"));
+            }
+            Intent intent = new Intent();
+            intent.putExtras(bundle);
+            setResult(RESULT_OK,intent);
+            finish();
+        });
+        binding.setViewModel(viewModel);
+//        binding.setEditClickListener(v -> {
+//            Toast.makeText(this, "Save btn clicked", Toast.LENGTH_SHORT).show();
+//        });
+//        binding.setEditClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.editBtn){
+            Toast.makeText(this, "Edit", Toast.LENGTH_SHORT).show();
+//            binding.getViewModel().isEdit = true;
+//            binding.invalidateAll();
+        }
+    }
+
+    private void save(String name, String phone){
+        SharedPreferences sp = getSharedPreferences("CONTACT",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("NAME",name);
+        editor.putString("PHONE",phone);
+        boolean res = editor.commit();
+    }
+
+    private void load(){
+        SharedPreferences sp = getSharedPreferences("CONTACT",MODE_PRIVATE);
+        String name = sp.getString("NAME","NoName");
+        String phone = sp.getString("PHONE","NoPhone");
+        Toast.makeText(this, name + " " + phone, Toast.LENGTH_SHORT).show();
+    }
+
+//    @Override
+//    public void onSave(Bundle bundle) {
+//        Intent intent = new Intent();
+//        intent.putExtras(bundle);
+//        setResult(RESULT_OK,intent);
+//        finish();
+//    }
+//
+//    @Override
+//    public void onRemove(Bundle bundle) {
+//        Intent intent = new Intent();
+//        intent.putExtras(bundle);
+//        setResult(RESULT_OK,intent);
+//        finish();
+//    }
+
+//    @Override
+//    public void onSave(String name, String email, String phone, String address, int position) {
+//        Intent intent = new Intent();
+//        intent.putExtra("NAME",name);
+//        intent.putExtra("EMAIL",email);
+//        intent.putExtra("PHONE",phone);
+//        intent.putExtra("ADDRESS",address);
+//        intent.putExtra("POSITION",position);
+//        intent.putExtra("ACTION", position < 0 ? "ADD" : "UPDATE");
+//        setResult(RESULT_OK,intent);
+//        finish();
+//    }
+//
+//    @Override
+//    public void onRemove(int position) {
+//        Intent intent = new Intent();
+//        intent.putExtra("ACTION","REMOVE");
+//        intent.putExtra("POSITION",position);
+//        setResult(RESULT_OK,intent);
+//        finish();
+//    }
+
+
 }
--- app/src/main/java/com/telran/a25_12_19_hw/ContactViewModel.java
+++ app/src/main/java/com/telran/a25_12_19_hw/ContactViewModel.java
@@ -1,12 +1,20 @@
 package com.telran.a25_12_19_hw;
 
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.databinding.ObservableBoolean;
+import androidx.lifecycle.MutableLiveData;
+
 public class ContactViewModel {
     public String name;
     public String email;
     public String phone;
     public String address;
     public int position;
-    public boolean isEdit;
+    public ObservableBoolean isEdit = new ObservableBoolean();
+    public MutableLiveData<Bundle> liveData = new MutableLiveData<>();
+//    private ViewModelListener listener;
 
     public ContactViewModel(String name,
                             String email,
@@ -18,6 +26,54 @@ public class ContactViewModel {
         this.phone = phone;
         this.address = address;
         this.position = position;
-        isEdit = position < 0;
+//        isEdit = position < 0;
+        isEdit.set(position < 0);
     }
+
+//    public void setListener(ViewModelListener listener) {
+//        this.listener = listener;
+//    }
+
+    public void setEdit(boolean edit) {
+//       isEdit = edit;
+        isEdit.set(edit);
+    }
+
+    public void save(){
+//        if(listener != null){
+//            listener.onSave(name,email,phone,address,position);
+            Bundle bundle = new Bundle();
+            bundle.putString("NAME",name);
+            bundle.putString("EMAIL",email);
+            bundle.putString("PHONE",phone);
+            bundle.putString("ADDRESS",address);
+            bundle.putInt("POSITION",position);
+            bundle.putString("ACTION",position < 0 ? "ADD":"UPDATE");
+            liveData.setValue(bundle);
+//            listener.onSave(bundle);
+//            listener.onAction(bundle);
+//        }
+    }
+
+    public void remove(){
+//        if(listener != null){
+//            listener.onRemove(position);
+            Bundle bundle = new Bundle();
+            bundle.putInt("POSITION",position);
+            bundle.putString("ACTION","REMOVE");
+            liveData.setValue(bundle);
+//            listener.onRemove(bundle);
+//            listener.onAction(bundle);
+//        }
+    }
+
+//    public static interface ViewModelListener{
+//        void onSave(String name, String email, String phone, String address, int position);
+//        void onRemove(int position);
+
+//        void onSave(Bundle bundle);
+//        void onRemove(Bundle bundle);
+
+//        void onAction(Bundle bundle);
+//    }
 }
